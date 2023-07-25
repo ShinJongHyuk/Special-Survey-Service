14ce05f61f7b25a543795ba289fc3ec3c45a8bed
0214kbs <0214kbs@naver.com>
2023-07-26 00:48:43 +0900

    Feat: Navbar 틀 작업

---
+++ frontend/src/components/common/navbar/Navbar.styled.tsx
@@ -0,0 +1,21 @@
+import styled from "styled-components";
+import Link from "next/link";
+
+const StyledNavbar = styled.div`
+  height: 56px;
+  background-color: ${(props) => props.theme.colors.yellow};
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 8px 30px;
+`;
+
+const StyledNavLink = styled(Link)`
+  padding: 0px 10px;
+  font-family: ${(props) => props.theme.fonts.HangeulFontSemiBold};
+  font-size: ${(props) => props.theme.fontSizes.small};
+  color: ${(props) => props.theme.colors.black};
+`;
+
+export { StyledNavbar, StyledNavLink };
--- frontend/src/components/common/navbar/index.tsx
+++ frontend/src/components/common/navbar/index.tsx
@@ -1,12 +1,27 @@
 "use client";
 import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+import { StyledNavbar, StyledNavLink } from "./Navbar.styled";
 
 const NavbarComponent = () => {
-
   return (
     <header>
-      <div style={{ height: "56px", backgroundColor: "#FFF129" }}>
-      </div>
+      <StyledNavbar>
+        <Link href="/">
+          <Image src="/Logo.svg" priority={true} width={21} height={21} alt="Logo"></Image>
+          <Image src="/SSS.svg" priority={true} width={47} height={16} alt="SSS"></Image>
+        </Link>
+
+        <div>
+          <StyledNavLink href="/">설문하기</StyledNavLink>
+          <StyledNavLink href="/">응답하기</StyledNavLink>
+        </div>
+        <div>
+          <StyledNavLink href="/">로그인</StyledNavLink>
+          <StyledNavLink href="/">회원가입</StyledNavLink>
+        </div>
+      </StyledNavbar>
     </header>
   );
 };
