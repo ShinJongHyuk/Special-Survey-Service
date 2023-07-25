dc971b164366141728af3d3d1b3c31bb27a9b57f
0214kbs@naver.com <0214kbs@naver.com>
2023-07-25 15:49:53 +0900

    Style: 글꼴 추가

--- frontend/src/styles/DefaultTheme.ts
+++ frontend/src/styles/DefaultTheme.ts
@@ -2,6 +2,9 @@ import { DefaultTheme } from "styled-components";
 
 const theme: DefaultTheme = {
   colors: {
+    yellowmain: "#FFF129",
+    purplemain: "#8E69FF",
+
   },
 };
 export default theme;
