4324722e7d2f91ff23ce288870113e59bbfe4235
0214kbs@naver.com <0214kbs@naver.com>
2023-07-25 12:09:25 +0900

    Frontend Project Setting

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
     <div>
-      <h2> home 화면!! </h2>
+      <h2> main </h2>
     </div>
   );
 }
--- frontend/src/components/common/navbar/index.tsx
+++ frontend/src/components/common/navbar/index.tsx
@@ -1,26 +1,12 @@
 "use client";
 import React from "react";
-import { StyledNav, StyledNavLink, NavLinks } from "./NavBar.styled";
-import Image from "next/image";
 
 const NavbarComponent = () => {
-  const onCreateAccount = () => {
-    console.log("create account");
-  };
 
   return (
     <header>
-      <StyledNav>
-        <StyledNavLink href="/">
-          <Image src="/cow.jpg" priority={true} width={50} height={50} alt="Logo"></Image>
-        </StyledNavLink>
-        <NavLinks>
-          <StyledNavLink href="/login">로그인</StyledNavLink>
-          <StyledNavLink href="/signup" onClick={onCreateAccount}>
-            회원가입
-          </StyledNavLink>
-        </NavLinks>
-      </StyledNav>
+      <div style={{ height: "56px", backgroundColor: "#FFF129" }}>
+      </div>
     </header>
   );
 };
