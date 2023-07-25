0574d6dab7fa6179a81563362b79bebe65553e4a
0214kbs <0214kbs@naver.com>
2023-07-25 23:01:07 +0900

    Style: ThemeProvider에 font style 추가

--- frontend/src/styles/DefaultTheme.ts
+++ frontend/src/styles/DefaultTheme.ts
@@ -4,7 +4,20 @@ const theme: DefaultTheme = {
   colors: {
     yellowmain: "#FFF129",
     purplemain: "#8E69FF",
-
+  },
+  fonts: {
+    HangeulFontMedium: "HangeulFontMedium",
+    HangeulFontRegular: "HangeulFontRegular",
+    HangeulFontSemiBold: "HangeulFontSemiBold",
+    EnglishFontBold: "EnglishFontBold",
+    EnglishFontLight: "EnglishFontLight",
+  },
+  fontSizes: {
+    xsmall: "12px",
+    small: "16px",
+    medium: "20px",
+    large: "32px",
+    xlarge: "48px",
   },
 };
 export default theme;
