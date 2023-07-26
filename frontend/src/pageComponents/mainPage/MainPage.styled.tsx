7b5956b615fe12bb04c24ab2558e84ffc4e38cea
0214kbs@naver.com <0214kbs@naver.com>
2023-07-26 14:32:32 +0900

    Feat: Banner 이미지 위에 text box 추가

--- frontend/src/pageComponents/mainPage/MainPage.styled.tsx
+++ frontend/src/pageComponents/mainPage/MainPage.styled.tsx
@@ -1,16 +1,53 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
-const StyledTextbox = styled.div`
+const StyledBanner = styled.div`
+  img{
+    width: 100%;
+    height: 500px;
+  }
+  
+`
+const StyledBannerBox = styled.div`
   display: inline-flex;
   flex-direction: column;
   align-items: center;
-  gap: 48px;
+  position: absolute;
+  top:40%;
+  left:50%;
+  transform: translate(-50%, -50%);
 
-  position: "absolute";
-  top: "50%";
-  left: "50%";
-  transform: "translate(-50%, -50%)";
-  z-index: 1;
+  gap:48px;
+`
+const StyledTextbox = styled.div`
+  display:flex;
+  flex-direction: column;
+  text-align: center;
+  gap: 16px;
+  width: 500px;
 `;
 
-export { StyledTextbox };
+interface TextType {
+  isKo: boolean;
+}
+
+const StyledBannerText = styled.div.attrs<TextType>((props) => ({}))`
+  ${(props) => {
+    const font = props.isKo ? props.theme.fonts.HangeulFontSemiBold : props.theme.fonts.EnglishFontLight;
+    const black = props.theme.colors.black;
+    const gray = props.theme.colors.gray;
+    const size = props.isKo ? props.theme.fontSizes.xlarge : props.theme.fontSizes.large;
+
+    return css`
+      width: 100%;
+      color: ${black};
+      font-family: ${font};
+      font-size: ${size};
+      text-align: center;
+      ${props.isKo && `
+    text-shadow: 1px 0 ${black}, -1px 0 ${black}, 0 1px ${black}, 0 -1px ${black}, 0 2px ${gray};
+  `}
+    `;
+  }};
+`;
+
+export { StyledTextbox, StyledBanner, StyledBannerText, StyledBannerBox };
--- frontend/src/pageComponents/mainPage/index.tsx
+++ frontend/src/pageComponents/mainPage/index.tsx
@@ -1,24 +1,22 @@
 "use client";
 import React from "react";
-import Image from "next/image";
-import { StyledTextbox } from "./MainPage.styled";
+import { StyledTextbox, StyledBannerBox, StyledBanner, StyledBannerText } from "./MainPage.styled";
 const MainPage = () => {
   return (
-    <div style={{ position: "relative" }}>
-      <div style={{ width: "100%", height: "450px", position: "relative" }}>
-        <Image src="/main.png" priority={true} fill alt="main"></Image>
-      </div>
-      <StyledTextbox>
-        <div>
-          <div> Let's Survey! </div>
-          <div> 우리는 설문조사를 하고 리워드를 받는 사이트에요!</div>
-        </div>
+    <StyledBanner>
+      <img src="/main.png"></img>
+
+      <StyledBannerBox>
+        <StyledTextbox>
+          <StyledBannerText isKo={false}> Let's Survey! </StyledBannerText>
+          <StyledBannerText isKo={true}> 우리는 설문조사를 하고 리워드를 받는 사이트에요!</StyledBannerText>
+        </StyledTextbox>
         <div>
           <button> 응답하기 </button>
           <button> 제작하기 </button>
         </div>
-      </StyledTextbox>
-    </div>
+      </StyledBannerBox>
+    </StyledBanner>
   );
 };
 
