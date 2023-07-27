cf72dae3469a1176a6962db1b7829d3ee1024ab4
전대현 <wjseogus523@naver.com>
2023-07-27 12:07:52 +0900

    Feat : 설문 컴포넌트 이미지 삽입 기능 구현

--- frontend/src/components/survey/multiplechoice/MultipleChoice.styled.tsx
+++ frontend/src/components/survey/multiplechoice/MultipleChoice.styled.tsx
@@ -14,7 +14,7 @@ const MultipleChoice_content_Box = styled.div.attrs({})`
     display : flex;
     flex-direction : row;
     align-items : center;
-    width : 600px;
+    width : 700px;
     height : 20px;
     
 `
@@ -54,14 +54,68 @@ const AddButton = styled.button.attrs({})`
 
 `
 const DeleteButton = styled.button.attrs({})`
-    display : flex;
-    border : none;
-    background-color : white;
-    font-size: ${props => props.theme.fontSizes.xsmall};
+  display: flex;
+  background-color: white;
+  color: ${props => props.theme.colors.purple};
+  border : none;
+  &:hover {
+
+    font-size: ${props => props.theme.fontSizes.medium};
     color: ${props => props.theme.colors.black};
+  }
+`;
 
-`
+const UploadImage = styled.input.attrs({type : 'file'})`
+    display : none;
+`;
+
+
+const Image_Delete_Button = styled.button`
+    content: 'X';
+    position: absolute;
+    top: -5px;
+    right: -5px;
+    background-color: ${props => props.theme.colors.purple};
+    color: white;
+    width: 20px;
+    height: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    font-weight: bold;
+    cursor: pointer;
+    opacity: 0; /* Hide the X button by default */
+    transition: opacity 0.2s ease-in-out; /* Add a transition effect for smooth visibility */
+`;
+
+const ImagePreiew_Box = styled.div`
+    display: flex;
+    position: relative;
+    max-width: 70px;
+    max-height: 70px;
+
+    &:hover {
+        ${Image_Delete_Button} {
+            opacity: 1; 
+        }
+    }
+`;
+
+const ImagePreview = styled.img`
+  max-width: 70px;
+  max-height: 70px;
+  border-radius : 6px;
+
+`;
+
+const ImageWrapper = styled.div`
+  position: relative;
+  cursor: pointer;
+
+`;
 
 
 
-export {DeleteButton,AddButton,MultipleChoice_content_Box,MultipleCheck,MultipleCheckText,MultipleChoice_Box}
+
+export {Image_Delete_Button,ImagePreiew_Box,ImageWrapper,UploadImage,ImagePreview,DeleteButton,AddButton,MultipleChoice_content_Box,MultipleCheck,MultipleCheckText,MultipleChoice_Box}
--- frontend/src/components/survey/multiplechoice/index.tsx
+++ frontend/src/components/survey/multiplechoice/index.tsx
@@ -1,48 +1,91 @@
 import React,{useState} from 'react';
 import MultipleChoiceType from './MultipleChoice.type';
-import {DeleteButton,AddButton,MultipleChoice_content_Box,MultipleChoice_Box,MultipleCheck,MultipleCheckText } from './MultipleChoice.styled';
+import Image from 'next/image'
+import ImageIcon from '/public/survey/ImageIcon.png'
+import {Image_Delete_Button,ImagePreiew_Box,ImageWrapper,UploadImage,ImagePreview,DeleteButton,AddButton,MultipleChoice_content_Box,MultipleChoice_Box,MultipleCheck,MultipleCheckText } from './MultipleChoice.styled';
 
 
 const MultipleChoice = () => {
     const [items, setItems] = useState<any[]>([
-        { id: Date.now(), text: '답변 1' },
-        { id: Date.now() + 1, text: '답변 2' },
-    ]); 
-
+      { id: Date.now(), text: '답변 1', imageUrl: '' },
+      { id: Date.now() + 1, text: '답변 2', imageUrl: '' },
+    ]);
+  
     const handleAddItem = () => {
-        setItems([...items, { id: Date.now(), text: '' }]);
+      setItems([...items, { id: Date.now(), text: '', imageUrl: '' }]);
     };
-
+  
     const handleDeleteItem = (index: number) => {
-        const updatedItems = [...items];
-        updatedItems.splice(index, 1);
-        setItems(updatedItems);
+      const updatedItems = [...items];
+      updatedItems.splice(index, 1);
+      setItems(updatedItems);
     };
+  
     const handleItemTextChange = (index: number, text: string) => {
-        const updatedItems = [...items];
-        updatedItems[index].text = text;
-        setItems(updatedItems);
+      const updatedItems = [...items];
+      updatedItems[index].text = text;
+      setItems(updatedItems);
     };
+
+    const handleImageClick = (index : number) => {
+        const uploadButton = document.getElementById(`upload-button-${index}`);
+
+
+        if (uploadButton) {
+          uploadButton.click();
+        }
+    };
+
+    const handleImageChange = (index: number, event: any) => {
+      const file = event.target.files[0];
+      console.log(file)
+      if (file) {
+        const imageUrl = URL.createObjectURL(file);
+        const updatedItems = [...items];
+        updatedItems[index].imageUrl = imageUrl;
+        setItems(updatedItems);
+      }
+      else { 
+        console.log("fuck")
+      }
+    };
+  
+    const handleImageDelete = (index: number) => {
+      const updatedItems = [...items];
+      updatedItems[index].imageUrl = '';
+      setItems(updatedItems);
+    };
+  
     return (
-        <MultipleChoice_Box>
-            {items.map((item, index) => (
-                <MultipleChoice_content_Box key={item.id}>
-                    <MultipleCheck name="radioGroup1" />
-                    <MultipleCheckText
-                        placeholder={`답변 ${index + 1}`}
-
-                        onChange={(e: React.ChangeEvent<HTMLInputElement>) => handleItemTextChange(index, e.target.value)}
-                    />
-                     {items.length > 1 && <DeleteButton onClick={() => handleDeleteItem(index)}>X</DeleteButton>}
-                     {index === items.length - 1 && (
-                        <AddButton onClick={() => handleAddItem()}>혹은 추가</AddButton>
-                     )}                
-                </MultipleChoice_content_Box>
-            ))}
-        </MultipleChoice_Box>
-
-    )
-}
-
-
-export default MultipleChoice
+      <MultipleChoice_Box>
+        {items.map((item, index) => (
+          <MultipleChoice_content_Box key={item.id}>
+            <MultipleCheck name="radioGroup1" />
+            <MultipleCheckText
+              placeholder={`답변 ${index + 1}`}
+              onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+                handleItemTextChange(index, e.target.value)
+              }
+            />
+            {item.imageUrl && (
+              <ImagePreiew_Box>
+                <ImagePreview src={item.imageUrl} alt={`Uploaded Image ${index + 1}`} />
+                <Image_Delete_Button onClick={() => handleImageDelete(index)}>X</Image_Delete_Button>
+              </ImagePreiew_Box>
+            )}
+  
+            <ImageWrapper onClick={() => handleImageClick(index)}>
+              <Image src={ImageIcon} alt="ImageIcon" />
+            </ImageWrapper>
+  
+            {index === items.length - 1 && <AddButton onClick={() => handleAddItem()}>문항 추가</AddButton>}
+  
+            <UploadImage id={`upload-button-${index}`} onChange={(e: any) => handleImageChange(index, e)} />
+            {items.length > 1 && <DeleteButton onClick={() => handleDeleteItem(index)}>X</DeleteButton>}
+          </MultipleChoice_content_Box>
+        ))}
+      </MultipleChoice_Box>
+    );
+  };
+  
+  export default MultipleChoice;
