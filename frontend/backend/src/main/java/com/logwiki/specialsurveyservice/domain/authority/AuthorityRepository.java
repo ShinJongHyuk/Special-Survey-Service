f4ad58de77d480fbb7fcb15023139f2e553698c9
0214kbs <0214kbs@naver.com>
2023-08-01 12:42:35 +0900

    Remove: files

--- frontend/backend/src/main/java/com/logwiki/specialsurveyservice/domain/authority/AuthorityRepository.java
+++
@@ -1,8 +0,0 @@
-package com.logwiki.specialsurveyservice.domain.authority;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorityRepository extends JpaRepository<Authority, Long> {
-
-    Authority findAuthorityByType(AuthorityType authorityType);
-}
--- frontend/frontend/src/components/Input/index.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-import { StyledInput } from "./Input.styled"
-const InputComponent = (props:any) => {
-    return (
-        <StyledInput {...props}></StyledInput>
-    )
-}
-
-export default InputComponent
