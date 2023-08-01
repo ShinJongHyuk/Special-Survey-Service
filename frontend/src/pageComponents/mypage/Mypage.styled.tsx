d7ff5b121788f357731e51ba6c014a1de4a54b5d
0214kbs <0214kbs@naver.com>
2023-08-01 23:34:43 +0900

    Feat: Mypage banner 작업중

---
+++ frontend/src/pageComponents/mypage/Mypage.styled.tsx
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+const StyledBanner = styled.div.attrs<any>((props) => ({}))`
+  display: flex;
+  padding-top: 60px;
+  gap: 100px;
+  justify-content: center;
+`;
+
+export { StyledBanner };
--- frontend/src/pageComponents/mypage/index.tsx
+++ frontend/src/pageComponents/mypage/index.tsx
@@ -2,14 +2,34 @@
 
 import { useMypageStore } from "@/stores/mypage/useMypageStore";
 import MypageComponents from "./mypageComponents";
+import useUserStore from "@/stores/useUserStore";
+import { useEffect } from "react";
+import Image from "next/image";
+import { StyledBanner } from "./Mypage.styled";
 
 const Mypage = () => {
   const selectBtn = useMypageStore((state) => state.selectBtn);
+  const userInformation = useUserStore((state: any) => state.userInformation);
+
+  useEffect(() => {
+    console.log(userInformation);
+  }, []);
 
   return (
     <>
       <div style={{ backgroundColor: "#FFF129", height: "220px" }}>
-        <div>Mypage</div>
+        <StyledBanner>
+          <Image src="/womanIcon.png" alt="man" width={120} height={120} style={{ border: "1px solid black", borderRadius: "50%" }}></Image>
+          <div>
+            <div>email</div>
+            <div>이름/생년월일</div>
+          </div>
+          <div>
+            <div> 응답한 설문</div>
+            <div> 만든 설문</div>
+            <div> 당첨된 상품 </div>
+          </div>
+        </StyledBanner>
       </div>
       <MypageComponents selectBtn={selectBtn} />
     </>
