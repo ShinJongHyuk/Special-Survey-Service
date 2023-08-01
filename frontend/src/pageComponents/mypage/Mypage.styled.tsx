51caa2db9b21a431319b6616e1d3ed147c3ea004
0214kbs <0214kbs@naver.com>
2023-08-02 00:26:07 +0900

    Feat: Mypage banner완성

--- frontend/src/pageComponents/mypage/Mypage.styled.tsx
+++ frontend/src/pageComponents/mypage/Mypage.styled.tsx
@@ -1,10 +1,42 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 const StyledBanner = styled.div.attrs<any>((props) => ({}))`
-  display: flex;
-  padding-top: 60px;
-  gap: 100px;
-  justify-content: center;
+  ${(props) => {
+    return css`
+      display: flex;
+      padding-top: 60px;
+      gap: 100px;
+      justify-content: center;
+      align-items: center;
+    `;
+  }}
 `;
 
-export { StyledBanner };
+const StyledText = styled.div.attrs<any>((props) => ({}))`
+  ${(props) => {
+    const fontsemibold = props.theme.fonts.HangeulFontSemiBold;
+    const fontregular = props.theme.fonts.HangeulFontRegular;
+    const medium = props.theme.fontSizes.medium;
+    const small = props.theme.fontSizes.small;
+    return css`
+      display: flex;
+      gap: 10px;
+      flex-direction: column;
+      .email {
+        font-family: ${fontsemibold};
+        font-size: ${medium};
+      }
+
+      .others {
+        font-family: ${fontregular};
+        font-size: ${small};
+      }
+
+      .survey {
+        font-family: ${fontregular};
+        font-size: ${small};
+      }
+    `;
+  }}
+`;
+export { StyledBanner, StyledText };
--- frontend/src/pageComponents/mypage/index.tsx
+++ frontend/src/pageComponents/mypage/index.tsx
@@ -1,34 +1,33 @@
 "use client";
-
 import { useMypageStore } from "@/stores/mypage/useMypageStore";
 import MypageComponents from "./mypageComponents";
 import useUserStore from "@/stores/useUserStore";
-import { useEffect } from "react";
 import Image from "next/image";
-import { StyledBanner } from "./Mypage.styled";
+import { StyledBanner, StyledText } from "./Mypage.styled";
 
 const Mypage = () => {
   const selectBtn = useMypageStore((state) => state.selectBtn);
   const userInformation = useUserStore((state: any) => state.userInformation);
 
-  useEffect(() => {
-    console.log(userInformation);
-  }, []);
-
   return (
     <>
-      <div style={{ backgroundColor: "#FFF129", height: "220px" }}>
+      <div style={{ backgroundColor: "#FFF129", height: "230px" }}>
         <StyledBanner>
-          <Image src="/womanIcon.png" alt="man" width={120} height={120} style={{ border: "1px solid black", borderRadius: "50%" }}></Image>
-          <div>
-            <div>email</div>
-            <div>이름/생년월일</div>
+          <div style={{ width: "30%", textAlign: "right" }}>
+            <Image src="/womanIcon.png" alt="man" width={120} height={120} style={{ border: "1px solid black", borderRadius: "50%" }}></Image>
           </div>
-          <div>
-            <div> 응답한 설문</div>
-            <div> 만든 설문</div>
-            <div> 당첨된 상품 </div>
-          </div>
+          <StyledText style={{ width: "30%" }}>
+            <div className="email">{userInformation.email}</div>
+            <div className="others">
+              {userInformation.name}/{userInformation.birthday}/
+              {userInformation.gender === "WOMAN" ? "여성" : userInformation.gender === "MAN" ? "남성" : userInformation.gender}
+            </div>
+          </StyledText>
+          <StyledText style={{ width: "45%" }}>
+            <div className="survey"> 응답한 설문 : {userInformation.responseSurveyCount}</div>
+            <div className="survey"> 만든 설문 : {userInformation.createSurveyCount}</div>
+            <div className="survey"> 당첨된 상품 : {userInformation.winningGiveawayCount}</div>
+          </StyledText>
         </StyledBanner>
       </div>
       <MypageComponents selectBtn={selectBtn} />
