dd1dbc2b009a0d85c1763ddd71c9ed484599f543
신종혁 <shinjh4903@gmail.com>
2023-08-03 00:35:06 +0900

    Feat : 설문 디테일 axios 진행 중

--- frontend/src/Hooks/useSurveyDetailHook.tsx
+++ frontend/src/Hooks/useSurveyDetailHook.tsx
@@ -1,14 +1,19 @@
 'use client'
 import { useEffect } from "react"
 import axios from 'axios'
+import useUserStore from "@/stores/useUserStore"
 
 export const useSurveyDetailHook = () => {
+    const accessToken = useUserStore((state:any) => state.accessToken)
     const survey_id = "1"
     const getSurveyDetail = async () => {
         try{
             const res = await axios({
                 method : 'get',
-                url : `http://221.164.64.185:8080/api/survey/detail/${survey_id}`
+                url : `http://221.164.64.185:8080/api/survey/detail/${survey_id}`,
+                // headers: {
+                //     Authorization: `Bearer ${accessToken}`
+                // }
             })
             console.log(res)
         } catch (err) {
