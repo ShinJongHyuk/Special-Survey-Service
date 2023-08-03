4ff535f1d2a698a16fb2f11cb726945701160ed2
0214kbs <0214kbs@naver.com>
2023-08-03 16:07:10 +0900

    Fix: Api 주소 수정

--- frontend/src/api/surveylist/instantListGet.ts
+++ frontend/src/api/surveylist/instantListGet.ts
@@ -3,11 +3,7 @@ import api from "../api";
 const instantListGet = async (accessToken: any) => {
   console.log("accessToken = " + accessToken);
   try {
-    const response = await api.get("/survey/recommend/instant", {
-      headers: {
-        Authorization: "Bearer " + accessToken,
-      },
-    });
+    const response = await api.get("/survey/recommend/instant/anonymous");
     console.log("instant: ", response.data.response)
     return response.data.response;
   } catch (error) {
--- frontend/src/api/surveylist/normalListGet.ts
+++ frontend/src/api/surveylist/normalListGet.ts
@@ -3,11 +3,7 @@ import api from "../api";
 const normalListGet = async (accessToken: any) => {
   console.log("accessToken = " + accessToken);
   try {
-    const response = await api.get("/survey/recommend/normal", {
-      headers: {
-        Authorization: "Bearer " + accessToken,
-      },
-    });
+    const response = await api.get("/survey/recommend/normal/anonymous");
     return response.data.response;
   } catch (error) {
     console.error("Error: ", error);
