0f49f474c3e7bca620116b72fcb322a80c90285b
slbin <smpts00@naver.com>
2023-08-03 16:39:18 +0900

    feat: 기타 수정 사항 추가

--- backend/src/main/java/com/logwiki/specialsurveyservice/api/service/surveyresult/SurveyResultService.java
+++ backend/src/main/java/com/logwiki/specialsurveyservice/api/service/surveyresult/SurveyResultService.java
@@ -2,7 +2,6 @@ package com.logwiki.specialsurveyservice.api.service.surveyresult;
 
 import com.logwiki.specialsurveyservice.api.controller.surveyresult.response.SurveyResultResponse;
 import com.logwiki.specialsurveyservice.api.service.account.AccountService;
-import com.logwiki.specialsurveyservice.api.service.survey.response.SurveyResponse;
 import com.logwiki.specialsurveyservice.api.service.surveyresult.response.MyGiveawayResponse;
 import com.logwiki.specialsurveyservice.api.service.surveyresult.response.ResultPageResponse;
 import com.logwiki.specialsurveyservice.domain.account.Account;
@@ -31,7 +30,7 @@ public class SurveyResultService {
     private final TargetNumberRepository targetNumberRepository;
     private final static boolean DEFAULT_WIN = false;
 
-    public void addSubmitResult(Long surveyId, LocalDateTime writeDateTime) {
+    public void addSubmitResult(Long surveyId, LocalDateTime answerDateTime) {
         Survey survey = surveyRepository.findById(surveyId)
                 .orElseThrow(() -> new BaseException("설문조사 PK가 올바르지 않습니다.", 3010));
         Account account = accountService.getCurrentAccountBySecurity();
@@ -46,7 +45,7 @@ public class SurveyResultService {
             throw new BaseException("이미 응답한 설문입니다.", 3012);
         }
 
-        SurveyResult surveyResult = SurveyResult.create(DEFAULT_WIN, writeDateTime, submitOrder, survey,
+        SurveyResult surveyResult = SurveyResult.create(DEFAULT_WIN, answerDateTime, submitOrder, survey,
                 account);
 
         if (survey.getSurveyCategory().getType().equals(SurveyCategoryType.INSTANT_WIN)) {
@@ -60,6 +59,7 @@ public class SurveyResultService {
         account.increaseResponseSurveyCount();
 
         surveyResultRepository.save(surveyResult);
+
 
         survey.addHeadCount();
     }
@@ -78,24 +78,12 @@ public class SurveyResultService {
                 .toList();
 
         return winSurveyResults.stream()
-                .map(surveyResult -> MyGiveawayResponse.of(surveyResult,
+                .map(surveyResult -> MyGiveawayResponse.of(
+                        surveyResult,
                         targetNumberRepository.findTargetNumberByNumberAndSurvey_Id(
                                 surveyResult.getSubmitOrder(),
-                                surveyResult.getSurvey().getId()).getGiveaway()))
-                .toList();
-    }
-
-    public List<SurveyResponse> getAnsweredSurveys() {
-        Account account = accountService.getCurrentAccountBySecurity();
-        List<SurveyResult> surveyResults = surveyResultRepository.findSurveyResultsByAccount_Id(
-                account.getId());
-
-        List<Survey> surveys = surveyResults.stream()
-                .map(SurveyResult::getSurvey)
-                .toList();
-
-        return surveys.stream()
-                .map(SurveyResponse::from)
+                                surveyResult.getSurvey().getId()).getGiveaway(),
+                        accountService.getUserNameById(surveyResult.getSurvey().getWriter())))
                 .toList();
     }
 
@@ -136,7 +124,7 @@ public class SurveyResultService {
         if (LocalDateTime.now().isBefore(survey.getEndTime())) {
             throw new BaseException("마감되지 않은 설문은 결과를 확인할수 없습니다.", 3016);
         }
-        
+
         SurveyResult surveyResult = surveyResultRepository.findSurveyResultBySurvey_IdAndAccount_Id(surveyId, account.getId());
 
         if (surveyResult == null) {
