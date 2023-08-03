5f0c34e76d141d14144e6758fb334271fe3a26dc
slbin <smpts00@naver.com>
2023-08-03 11:10:14 +0900

    feat: 문항응답시 검증 로직 추가

--- backend/src/main/java/com/logwiki/specialsurveyservice/api/service/survey/SurveyService.java
+++ backend/src/main/java/com/logwiki/specialsurveyservice/api/service/survey/SurveyService.java
@@ -168,4 +168,19 @@ public class SurveyService {
     private static void sortByRequiredTimeForSurvey(List<Survey> surveys) {
         surveys.sort(Comparator.comparingInt(Survey::getRequiredTimeInSeconds));
     }
+
+    public SurveyResponse getSurvey(Long surveyId) {
+        return SurveyResponse.from(surveyRepository.findById(surveyId)
+                .orElseThrow(() -> new BaseException("없는 설문입니다.", 3005)));
+    }
+
+    public List<SurveyResponse> getMySurveys() {
+        Account account = accountService.getCurrentAccountBySecurity();
+        List<Survey> mySurveys = surveyRepository.findAllByWriter(account.getId());
+
+        return mySurveys.stream()
+                .map(SurveyResponse::from)
+                .toList();
+
+    }
 }
--- backend/src/main/java/com/logwiki/specialsurveyservice/api/service/surveyresult/SurveyResultService.java
+++ backend/src/main/java/com/logwiki/specialsurveyservice/api/service/surveyresult/SurveyResultService.java
@@ -1,16 +1,21 @@
 package com.logwiki.specialsurveyservice.api.service.surveyresult;
 
 import com.logwiki.specialsurveyservice.api.service.account.AccountService;
+import com.logwiki.specialsurveyservice.api.service.survey.response.SurveyResponse;
+import com.logwiki.specialsurveyservice.api.service.surveyresult.response.MyGiveawayResponse;
 import com.logwiki.specialsurveyservice.domain.account.Account;
 import com.logwiki.specialsurveyservice.domain.survey.Survey;
 import com.logwiki.specialsurveyservice.domain.survey.SurveyRepository;
+import com.logwiki.specialsurveyservice.domain.surveycategory.SurveyCategoryType;
 import com.logwiki.specialsurveyservice.domain.surveyresult.SurveyResult;
 import com.logwiki.specialsurveyservice.domain.surveyresult.SurveyResultRepository;
+import com.logwiki.specialsurveyservice.domain.targetnumber.TargetNumberRepository;
 import com.logwiki.specialsurveyservice.exception.BaseException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -19,12 +24,13 @@ public class SurveyResultService {
     private final SurveyResultRepository surveyResultRepository;
     private final SurveyRepository surveyRepository;
     private final AccountService accountService;
+    private final TargetNumberRepository targetNumberRepository;
 
     public void addSubmitResult(Long surveyId, LocalDateTime writeDateTime) {
         Survey survey = surveyRepository.findById(surveyId)
                 .orElseThrow(() -> new BaseException("설문조사 PK가 올바르지 않습니다.", 3010));
         Account account = accountService.getCurrentAccountBySecurity();
-        SurveyResult checkSurveyResult = surveyResultRepository.findSurveyResultByAccount_Id(account.getId());
+        SurveyResult checkSurveyResult = surveyResultRepository.findSurveyResultBySurvey_IdAndAccount_Id(survey.getId(), account.getId());
 
         int submitOrder = createSubmitOrderIn(surveyId);
 
@@ -35,14 +41,17 @@ public class SurveyResultService {
             throw new BaseException("이미 응답한 설문입니다.", 3012);
         }
 
-        boolean isWin = survey.getTargetNumbers().stream()
-                .anyMatch(targetNumber -> targetNumber.getNumber() == submitOrder);
-        if (isWin) {
-            account.increaseWinningGiveawayCount();
+        boolean isWin = false;
+        if (survey.getSurveyCategory().getType().equals(SurveyCategoryType.INSTANT_WIN)) {
+            isWin = survey.getTargetNumbers().stream()
+                    .anyMatch(targetNumber -> targetNumber.getNumber() == submitOrder);
+            if (isWin) {
+                account.increaseWinningGiveawayCount();
+            }
         }
 
         account.increaseResponseSurveyCount();
-        SurveyResult surveyResult = surveyResultRepository.save(SurveyResult.create(isWin, writeDateTime, submitOrder, survey,
+        surveyResultRepository.save(SurveyResult.create(isWin, writeDateTime, submitOrder, survey,
                 account));
 
         survey.addHeadCount();
@@ -50,5 +59,36 @@ public class SurveyResultService {
 
     public int createSubmitOrderIn(Long surveyId) {
         return surveyResultRepository.findSubmitCountBy(surveyId) + 1;
+    }
+
+    public List<MyGiveawayResponse> getMyGiveaways() {
+        Account account = accountService.getCurrentAccountBySecurity();
+        List<SurveyResult> surveyResults = surveyResultRepository.findSurveyResultsByAccount_Id(
+                account.getId());
+
+        List<SurveyResult> winSurveyResults = surveyResults.stream()
+                .filter(SurveyResult::isWin)
+                .toList();
+
+        return winSurveyResults.stream()
+                .map(surveyResult -> MyGiveawayResponse.of(surveyResult,
+                        targetNumberRepository.findTargetNumberByNumberAndSurvey_Id(
+                                surveyResult.getSubmitOrder(),
+                                surveyResult.getSurvey().getId()).getGiveaway()))
+                .toList();
+    }
+
+    public List<SurveyResponse> getAnsweredSurveys() {
+        Account account = accountService.getCurrentAccountBySecurity();
+        List<SurveyResult> surveyResults = surveyResultRepository.findSurveyResultsByAccount_Id(
+                account.getId());
+
+        List<Survey> surveys = surveyResults.stream()
+                .map(SurveyResult::getSurvey)
+                .toList();
+
+        return surveys.stream()
+                .map(SurveyResponse::from)
+                .toList();
     }
 }
