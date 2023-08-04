46ff50c666d379f640c83bbdd3c88ab4cae148dd
신종혁 <shinjh4903@gmail.com>
2023-08-04 16:18:14 +0900

    Feat : 연계질문 기능구현

--- frontend/src/components/surveyanswer/shortfrom/index.tsx
+++ frontend/src/components/surveyanswer/shortfrom/index.tsx
@@ -1,8 +1,55 @@
 'use client'
+import { useState, useEffect } from 'react'
 import { ShortInput } from './ShortForm.styled'
-const ShortFormComponent = () => {
+import useSurveyAnswerStore from '@/stores/useSurveyAnswer'
+
+interface resultType {
+    questionId : string
+    shortFormAnswer : string
+}
+
+const ShortFormComponent = (props:any) => {
+    const questionNumber = props.questionNumber
+    const [result, setResult] = useState<resultType>({
+        questionId : "",
+        shortFormAnswer : ""
+    })
+
+    const setAnswer = useSurveyAnswerStore((state:any) => state.setAnswer)
+    const removeAnswer = useSurveyAnswerStore((state:any) => state.removeAnswer)
+    const answer = useSurveyAnswerStore((state:any) => state.answer)
+
+    const linkNumber = useSurveyAnswerStore((state:any) => state.linkNumber)
+    const [ disabled, setDisabled] = useState(true)
+
+    useEffect(() => {
+        if (result.shortFormAnswer.length === 0) {
+            removeAnswer(result)
+        }
+    },[result])
+
+    useEffect(() => {
+        if (linkNumber.includes(questionNumber)) {
+            setDisabled(true)
+        } else {
+            setDisabled(false)
+        }
+    },[linkNumber])
+
+    const onChange = (e:any) => {
+        const updateResult = {
+            ...result,
+            ["questionId"] : String(questionNumber),
+            ["shortFormAnswer"] : e.target.value
+        }
+        setResult(updateResult)
+        setAnswer(updateResult)
+    }
+
+    
+    
     return (
-        <ShortInput placeholder="단답형 텍스트"/>
+        <ShortInput disabled={disabled} placeholder="단답형 텍스트" onChange={onChange}/>
     )
 }
 
--- frontend/src/stores/useSurveyAnswer.tsx
+++ frontend/src/stores/useSurveyAnswer.tsx
@@ -13,7 +13,37 @@ const useSurveyAnswerStore = create(
             
         }),
         linkNumber : [],
-        setLinkNumber : (data:any) => set((state:any) => (state.linkNumber.push(data)))
+        setLinkNumber: (data:any) =>
+        set((state:any) => {
+            if (!state.linkNumber.includes(data)) {
+                return {
+                    linkNumber: [...state.linkNumber, data],
+                };
+            }
+            return state;
+        }),
+        // setLinkNumber : (data:any) => set((state:any) => ({
+        //     linkNumber : [...state.linkNumber, data]
+        // })),
+
+        removeLinkNumber : (data:any) => set((state:any) => {
+            const NewLinkNumber = state.linkNumber.filter((element:any) => {
+                return element != data
+            })
+            return {
+                linkNumber : [...NewLinkNumber]
+            }
+        }),
+
+        removeAnswer : (data:any) => set((state:any) => {
+            const newAnswer = state.answer.filter((element:any) => {
+                return element.questionId !== data.questionId
+            })
+            return {
+                answer : [...newAnswer]
+            }
+        })
+
     }))
 )
 
