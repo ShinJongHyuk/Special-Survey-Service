cd7306bada6f9e5d143df6843ace5b82b1d51f06
slbin <smpts00@naver.com>
2023-08-04 09:30:11 +0900

    .

--- frontend/src/api/api.ts
+++ frontend/src/api/api.ts
@@ -6,7 +6,7 @@ const api = axios.create({
 
 api.interceptors.request.use(
   async (request) => {
-    console.log(process.env.NEXT_PUBLIC_API_URL);
+    console.log("URL = "+process.env.NEXT_PUBLIC_API_URL);
     return request;
   },
   (error) => {
