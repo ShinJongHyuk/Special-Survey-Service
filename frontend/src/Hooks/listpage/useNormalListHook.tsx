7f733f98faf2887c7d1596a565de46c6e873061a
0214kbs <0214kbs@naver.com>
2023-08-04 09:34:18 +0900

    Feat: 로그인 여부에 따라 설문 리스트 요청

--- frontend/src/Hooks/listpage/useNormalListHook.tsx
+++ frontend/src/Hooks/listpage/useNormalListHook.tsx
@@ -3,14 +3,23 @@ import useTimerHook from "@/Hooks/card/useTimerHook";
 import normalListGet from "@/api/surveylist/normalListGet";
 import useSortSurveyListHook from "./useSortSurveyListHook";
 import { useSortTypeStore } from "@/stores/surveylist/useSortTypeStore";
+import userNormalListGet from "@/api/surveylist/userNormalListGet";
 
 const useNormalListHook = () => {
   const [surveys, setSurveys] = useState<any>([]);
 
   useEffect(() => {
     const fetchList = async () => {
-      const data = await normalListGet();
-      setSurveys(data);
+      const accessToken = localStorage.getItem('accessToken');
+
+      if (accessToken === null) {
+        const data = await normalListGet();
+        setSurveys(data);
+      } else {
+        const data = await userNormalListGet(accessToken);
+        setSurveys(data);
+      }
+
     };
     fetchList();
   }, []);
