2098c1848258825974ebcf6e10ffb1b204436779
0214kbs <0214kbs@naver.com>
2023-08-05 19:58:59 +0900

    Fix: 로그인 여부 에 따라 접근 관련 코드 제거

--- frontend/src/pageComponents/mypage/index.tsx
+++ frontend/src/pageComponents/mypage/index.tsx
@@ -2,36 +2,26 @@
 import { useMypageStore } from "@/stores/mypage/useMypageStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import useUserStore from "@/stores/useUserStore";
 import MypageComponents from "./mypageComponents";
 import Banner from "./mypageComponents/components/banner";
+
 // import dynamic from "next/dynamic";
 // const Banner = dynamic(() => import('./mypageComponents/components/banner'), { ssr: false })
 // const MypageComponents = dynamic(() => import('./mypageComponents'), { ssr: false })
 
 const Mypage = () => {
-  const isLogin = useUserStore((state: any) => state.isLogin);
   const router = useRouter();
-  useEffect(() => {
-    if (!isLogin) {
-      router.push("/login");
-    }
-  }, [isLogin]);
 
   const selectBtn = useMypageStore((state) => state.selectBtn);
 
-
   return (
     <div>
-
-      {isLogin ? (
-        <>
-          <Banner />
-          <MypageComponents selectBtn={selectBtn} />
-        </>
-      ) : <></>}
+      <>
+        <Banner />
+        <MypageComponents selectBtn={selectBtn} />
+      </>
     </div>
-  )
+  );
 };
 
 export default Mypage;
