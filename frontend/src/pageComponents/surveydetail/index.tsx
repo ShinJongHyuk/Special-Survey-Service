d3e8b7e01b37b5ca14aa0933abe0b72c5428fb08
0214kbs <0214kbs@naver.com>
2023-08-06 19:47:54 +0900

    Feat: survey detail 전체 layout

--- frontend/src/pageComponents/surveydetail/index.tsx
+++ frontend/src/pageComponents/surveydetail/index.tsx
@@ -1,32 +1,44 @@
-'use client'
-import Image from "next/image"
-import { SurveyDetailPage, BackButtonContainer, BackButton, SurveyResultContainer } from "./SurveyDetail.styled"
-import Board from './components/board'
-import Detail from './components/detail'
-import Result from './components/result'
-import useSurveyDetailHook from "@/Hooks/useSurveyDetailHook"
+"use client";
+import Image from "next/image";
+import Board from "./components/board";
+import Detail from "./components/detail";
+import Result from "./components/result";
+import useSurveyDetailHook from "@/Hooks/useSurveyDetailHook";
+import { StyledSurveyResultContainer } from "./SurveyDetail.styled";
+import { DetailProps } from "./components/detail/Detail.type";
 
 const SurveyDetail = (props: any) => {
-    console.log("SurveyDetail: " + props.id);
+  console.log("SurveyDetail: " + props.id);
 
-    const { surveyDetail } = useSurveyDetailHook(props.id);
-    console.log(surveyDetail);
-    return (
-        <SurveyDetailPage>
-            <BackButtonContainer>
-                <BackButton>
-                    <Image src="/surveyDetail/BackImg.png" alt="back" width={48} height={48}></Image>
-                </BackButton>
-            </BackButtonContainer>
+  const { surveyDetail } = useSurveyDetailHook(props.id);
+  console.log(surveyDetail);
 
-            <Detail type="즉시당첨"></Detail>
+  const detailProps: DetailProps = {
+    closedheadcount: surveyDetail.closedHeadCount,
+    endtime: surveyDetail.endTime,
+    headcount: surveyDetail.headCount,
+    questioncount: surveyDetail.questionCount,
+    requiredtime: surveyDetail.requiredTimeInSeconds,
+    starttime: surveyDetail.startTime,
+    type: surveyDetail.surveyCategoryType,
+    title: surveyDetail.title,
+    writername: surveyDetail.writerName,
+    giveawaynames: surveyDetail.giveawayNames,
+  };
 
-            <SurveyResultContainer>
-                <Result type="즉시당첨"></Result>
-                <Board type="즉시당첨"></Board>
-            </SurveyResultContainer>
-        </SurveyDetailPage>
-    )
-}
+  return (
+    <div style={{ backgroundColor: "#FAFAFA" }}>
+      <div style={{ paddingTop: "70px", paddingLeft: "30px" }}>
+        <Image src="/surveyDetail/BackImg.png" alt="back" width={48} height={48} style={{ cursor: "pointer" }}></Image>
+      </div>
+
+      <Detail {...detailProps}></Detail>
+      <StyledSurveyResultContainer>
+        <Result type="즉시당첨"></Result>
+        <Board type="즉시당첨"></Board>
+      </StyledSurveyResultContainer>
+    </div>
+  );
+};
 
 export default SurveyDetail;
