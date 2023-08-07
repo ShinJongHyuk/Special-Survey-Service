ede95e33e361205927886cc575a58c95b52c0f71
0214kbs <0214kbs@naver.com>
2023-08-07 11:28:56 +0900

    Feat: 설문 디테일 페이지 상단, 중간부분 데이터 처리 변경

--- frontend/src/api/surveydetail/surveyDetailGet.ts
+++ frontend/src/api/surveydetail/surveyDetailGet.ts
@@ -2,18 +2,19 @@ import api from "../api";
 
 const surveyDetailGet = async (id: any) => {
   try {
-    // console.log(id);
     const accessToken = localStorage.getItem("accessToken");
+    console.log("at : ", accessToken);
     const response = await api.get("/survey/detail/" + id, {
       headers: {
         Authorization: `Bearer ${accessToken}`,
       },
     });
-    if (response.data.success) {
-      return response.data.response;
-    } else {
-      console.log("error: ", response.data.apiError.message);
-    }
+    return response.data;
+    // if (response.data.success) {
+    //   console.log("survey detail get : ", response.data.response);
+    // } else {
+    //   console.log("error: ", response.data.apiError.message);
+    // }
   } catch (error) {
     console.error("Error: ", error);
     throw error;
--- frontend/src/pageComponents/surveydetail/SurveyDetailType.type.ts
+++ frontend/src/pageComponents/surveydetail/SurveyDetailType.type.ts
@@ -1,15 +1,21 @@
-export type DetailType = {
-  closedheadcount?: number;
-  endtime?: string;
-  headcount?: number;
-  questioncount?: number;
-  requiredtime?: number;
-  starttime?: string;
-  type?: string;
-  title?: string;
-  writername?: string;
-  giveawaynames?: string;
-  giveaways?: string;
-  percent?: string;
-  content?: string;
-};
+
+export interface DetailPropsType {
+  closedheadcount: string;
+  endtime: string;
+  headcount: string;
+  questioncount: string;
+  requiredtime: string;
+  starttime: string;
+  type: string;
+  title: string;
+  writername: string;
+  giveawaynames: string;
+  content: string;
+}
+
+export interface ResultPropsType {
+  headcount: string;
+  type: string;
+  giveawaytype: string;
+  percent: string;
+}
