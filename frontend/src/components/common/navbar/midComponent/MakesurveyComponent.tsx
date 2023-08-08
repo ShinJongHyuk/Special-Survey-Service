344eb9fd0bec6ca540e32792a4805583435447f3
전대현 <wjseogus523@naver.com>
2023-08-08 14:57:15 +0900

    Feat : 설문 제작 페이지 유효성 검사 추가

--- frontend/src/components/common/navbar/midComponent/MakesurveyComponent.tsx
+++ frontend/src/components/common/navbar/midComponent/MakesurveyComponent.tsx
@@ -65,32 +65,45 @@ const MakesruveyComponent = (props: any) => {
           count : item.price
         }))
         };
-        const Inner_hasEmptyValue = surveyData.questions.some((questionData : any) => {
-           
-            if (questionData.title === "" || questionData.content === "" || questionData.type === "") {
+        const Inner_hasEmptyValue = surveyData.questions.some((questionData : any, questionIndex : number) => {
+          if (questionData.title === "" || questionData.type === "") {
+              alert(`질문 ${questionIndex + 1}의 모든 필드를 채워주시기 바랍니다.`);
               return true;
-            }
-            if (questionData.multipleChoices) {
-              return questionData.multipleChoices.some((choice : any) => choice.content === "");
-            }           
-            if (questionData.checkBox) {
-              return questionData.checkBox.some((checkbox : any) => checkbox.content === "");
-            }
-            return false;
-          });
+          }
+          if (questionData.multipleChoices) {
+              const emptyChoiceIndex = questionData.multipleChoices.findIndex((choice : any) => choice.content === "");
+              if (emptyChoiceIndex !== -1) {
+                  alert(`질문 ${questionIndex + 1}의 선택지 ${emptyChoiceIndex + 1}의 내용을 채워주시기 바랍니다.`);
+                  return true;
+              }
+          }           
+          return false;
+        });
 
         const Outer_hasEmptyValue =
-            surveyData.title === "" ||
-            surveyData.closedHeadCount === 0 ||
-            surveyData.endTime === 0 ||
-            surveyData.startTime === 0 ||
-            (surveyData.surveyTarget === undefined || surveyData.surveyTarget.length === 0) ||
-            surveyData.type === ""
-        
-        if (Inner_hasEmptyValue || Outer_hasEmptyValue) {
-            alert("모든 필드를 채워주시기 바랍니다.");
-            return;
-        }
+          surveyData.title === "" ?
+          alert("설문 제목을 입력해주세요.") :
+          surveyData.closedHeadCount === 0 ?
+          alert("설문 인원을 입력해주세요.") :
+          surveyData.endTime === 0 ?
+          alert("종료 시간을 선택해주세요.") :
+          surveyData.startTime === 0 ?
+          alert("시작 시간을 선택해주세요.") :
+          (surveyData.surveyTarget === undefined || surveyData.surveyTarget.length === 0) ?
+          alert("설문 대상을 선택해주세요.") :
+          surveyData.type === "" ?
+          alert("설문 유형을 선택해주세요.") :
+          surveyData.startTime === "" ?
+          alert("설문 시작 시간을 선택해주세요.") :
+          surveyData.endTime === "" ?
+          alert("설문 마감 시간을 선택해주세요.") :
+          false;
+      
+      if (Inner_hasEmptyValue || Outer_hasEmptyValue) {
+        //여기서 결제 페이지로 이동
+        //아니면 결제페이지 말고 return 
+          return;
+      }
         console.log(surveyData)
         makeSurveyPost(surveyData)
    
