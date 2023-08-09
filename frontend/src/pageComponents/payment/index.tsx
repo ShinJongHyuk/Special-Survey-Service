96b424809477b8db02e262ffd3748d2dda2a90b9
slbin <smpts00@naver.com>
2023-08-09 15:39:25 +0900

    asd

--- frontend/src/pageComponents/payment/index.tsx
+++ frontend/src/pageComponents/payment/index.tsx
@@ -1,12 +1,12 @@
 "use client";
-import React, {useState,useEffect} from 'react'
-import {Selected_Box,SelectBox,SelectBox_List,SelectBox_Option,Info_LR_Box,Image_Wrapper,Info_Top_Box,Info_Bottom_Box,Info_Inner_Box,Bottom_Container,Top_Container,Main_Inner_Container, Title_Text,Information_Container, Main_Container, Pay_Container } from './Payment.Styled';
+import React, { useState, useEffect } from 'react'
+import { Selected_Box, SelectBox, SelectBox_List, SelectBox_Option, Info_LR_Box, Image_Wrapper, Info_Top_Box, Info_Bottom_Box, Info_Inner_Box, Bottom_Container, Top_Container, Main_Inner_Container, Title_Text, Information_Container, Main_Container, Pay_Container } from './Payment.Styled';
 import useSettingSurveyApiStore from "@/stores/makesurvey/useSettingSurveyApiStore";
 import useMakeSurveyApiStore from "@/stores/makesurvey/useMakeSurveyApiStore";
 import makeSurveyPost from "@/api/makesurvey/makeSurveyPost";
 import useSurveyStore from "@/stores/makesurvey/useSurveyStore";
 import Image from 'next/image'
-import giveawayListGet from "@/api/givawaylist/giveAwayListGet";
+import giveawayListGet from "@/api/givawaylist/giveawayListGet";
 import Woman_Img from '/public/payment/Woman_Img.svg'
 import Target from '/public/survey/Target.png'
 import Calendar from '/public/survey/Calendar.png'
@@ -17,7 +17,7 @@ import Kite from '/public/payment/kite.svg'
 import Button from '@/components/button';
 import ItemBox from '@/components/ItemBox';
 import usePriceStore from '@/stores/usePriceStore';
-import {v4 as uuidv4} from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
 function Payment(props: any) {
   const {
@@ -31,11 +31,11 @@ function Payment(props: any) {
     surveyTarget,
     img,
   } = useSettingSurveyApiStore();
-  const {price,increment,decrement} = usePriceStore();
-  const {surveyList} = useMakeSurveyApiStore();
-  const {surveyComponents} = useSurveyStore();
-  const [giveawaydata,setGiveaWayData] = useState([])
-  const [selectedOption,setSelectedOption] = useState<any[]>([])
+  const { price, increment, decrement } = usePriceStore();
+  const { surveyList } = useMakeSurveyApiStore();
+  const { surveyComponents } = useSurveyStore();
+  const [giveawaydata, setGiveaWayData] = useState([])
+  const [selectedOption, setSelectedOption] = useState<any[]>([])
 
   useEffect(() => {
     const fetchList = async () => {
@@ -43,10 +43,10 @@ function Payment(props: any) {
       setGiveaWayData(data);
     };
     fetchList();
-    
-    }, []);
 
-  const handlePaymentButtonClick = () => { 
+  }, []);
+
+  const handlePaymentButtonClick = () => {
     const surveyData = {
       title,
       titleContent,
@@ -60,105 +60,105 @@ function Payment(props: any) {
         const { componentKey, ...dataWithoutComponentKey } = surveyList[component.componentKey];
         return {
           ...dataWithoutComponentKey,
-          questionNumber: index + 1 
-          };
-        })
+          questionNumber: index + 1
+        };
+      })
         .filter(dataWithoutComponentKey => dataWithoutComponentKey !== undefined),
-      giveaways : giveawaydata.map((item : any) => ({
-        id : item.id,
-        count : item.price
+      giveaways: giveawaydata.map((item: any) => ({
+        id: item.id,
+        count: item.price
       }))
-      };
-      console.log(surveyData)
-      makeSurveyPost(surveyData)
+    };
+    console.log(surveyData)
+    makeSurveyPost(surveyData)
       .then((responseData) => {
         console.log("설문 제출에 성공하였습니다:", responseData);
         if (responseData) {
           console.log("데이터 이떠")
         }
-        })
-        .catch((error) => {
+      })
+      .catch((error) => {
         console.error("설문 제출에 실패하였습니다", error);
-        });
-    }
+      });
+  }
 
-    const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-      const selectedIndex = event.target.selectedIndex;
-      const selectedData = giveawaydata[selectedIndex - 1];
-      const componentKey = uuidv4();
-      setSelectedOption((prevSelectedOption) => [
-        ...prevSelectedOption,
-        { option: selectedData, componentKey, countKey: componentKey },
-      ]);
-    };
-  
-    const handleCountChange = (countKey: any, newCount: number) => {
+  const handleOptionChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+    const selectedIndex = event.target.selectedIndex;
+    const selectedData = giveawaydata[selectedIndex - 1];
+    const componentKey = uuidv4();
+    setSelectedOption((prevSelectedOption) => [
+      ...prevSelectedOption,
+      { option: selectedData, componentKey, countKey: componentKey },
+    ]);
+  };
+
+  const handleCountChange = (countKey: any, newCount: number) => {
+    setSelectedOption((prevSelectedOption) =>
+      prevSelectedOption.map((selected) =>
+        selected.countKey === countKey
+          ? { ...selected, count: newCount }
+          : selected
+      )
+    );
+  };
+
+  const handleOptionRemove = (indexToRemove: number) => {
+    const removedOption = selectedOption[indexToRemove];
+    if (removedOption) {
       setSelectedOption((prevSelectedOption) =>
-        prevSelectedOption.map((selected) =>
-          selected.countKey === countKey
-            ? { ...selected, count: newCount }
-            : selected
-        )
+        prevSelectedOption.filter((_, index) => index !== indexToRemove)
       );
-    };
-  
-    const handleOptionRemove = (indexToRemove: number) => {
-      const removedOption = selectedOption[indexToRemove];
-      if (removedOption) {
-        setSelectedOption((prevSelectedOption) =>
-          prevSelectedOption.filter((_, index) => index !== indexToRemove)
-        );
-        decrement(removedOption.option.price * removedOption.count);
-      }
-    };
-    
+      decrement(removedOption.option.price * removedOption.count);
+    }
+  };
+
 
   return (
     <Main_Container>
       <Main_Inner_Container>
         <Top_Container>
-          <Title_Text>결제 Page</Title_Text> 
-          <Image src={Woman_Img} width={170} height={150} alt="컴퓨터 하는 여자" style={{display : 'flex', alignItems : 'flex-end' , marginTop : "32px"}} />
+          <Title_Text>결제 Page</Title_Text>
+          <Image src={Woman_Img} width={170} height={150} alt="컴퓨터 하는 여자" style={{ display: 'flex', alignItems: 'flex-end', marginTop: "32px" }} />
         </Top_Container>
         <Bottom_Container>
           <Information_Container>
             <Info_LR_Box>
-              <Info_Inner_Box style={{display : "flex", flexDirection : "row",fontSize: '22px', height : "12%"}}>세부정보</Info_Inner_Box>
+              <Info_Inner_Box style={{ display: "flex", flexDirection: "row", fontSize: '22px', height: "12%" }}>세부정보</Info_Inner_Box>
               <Info_Inner_Box>
-                  <Info_Top_Box>
-                    <Image src={People} alt="인원" style={{marginRight : "12px"}}/>
-                    설문 인원
-                  </Info_Top_Box>
-                  <Info_Bottom_Box>{closedHeadCount}명</Info_Bottom_Box>
-                </Info_Inner_Box>
-                <Info_Inner_Box>
-                  <Info_Top_Box>
-                    <Image src={Target} alt="타겟" style={{marginRight : "12px"}} />
-                    설문 대상
-                  </Info_Top_Box>
-                  {surveyTarget && surveyTarget.map((item, index) => (
-                     <Info_Bottom_Box key={index}>{item}</Info_Bottom_Box>
-                  ))}
-                </Info_Inner_Box>
-                <Info_Inner_Box>
-                  <Info_Top_Box>
-                    <Image src={Category} alt="유형" style={{marginRight : "12px"}} />
-                    설문 유형
-                  </Info_Top_Box>
-                  <Info_Bottom_Box>{type}</Info_Bottom_Box>
-                </Info_Inner_Box>
-                <Info_Inner_Box>
-                  <Info_Top_Box>
-                    <Image src={Calendar} alt="기간" style={{marginRight : "12px"}} />
-                    설문 날짜
-                  </Info_Top_Box>
-                  <Info_Bottom_Box>{startTime} ~ {endTime}</Info_Bottom_Box>
-                </Info_Inner_Box>
-              </Info_LR_Box>
-              <Image_Wrapper>
-                <Image src={Kite} alt="연"style={{transform: "rotate(30deg)",width: "150px",marginLeft : "300px",marginBottom : "20%"}} />
+                <Info_Top_Box>
+                  <Image src={People} alt="인원" style={{ marginRight: "12px" }} />
+                  설문 인원
+                </Info_Top_Box>
+                <Info_Bottom_Box>{closedHeadCount}명</Info_Bottom_Box>
+              </Info_Inner_Box>
+              <Info_Inner_Box>
+                <Info_Top_Box>
+                  <Image src={Target} alt="타겟" style={{ marginRight: "12px" }} />
+                  설문 대상
+                </Info_Top_Box>
+                {surveyTarget && surveyTarget.map((item, index) => (
+                  <Info_Bottom_Box key={index}>{item}</Info_Bottom_Box>
+                ))}
+              </Info_Inner_Box>
+              <Info_Inner_Box>
+                <Info_Top_Box>
+                  <Image src={Category} alt="유형" style={{ marginRight: "12px" }} />
+                  설문 유형
+                </Info_Top_Box>
+                <Info_Bottom_Box>{type}</Info_Bottom_Box>
+              </Info_Inner_Box>
+              <Info_Inner_Box>
+                <Info_Top_Box>
+                  <Image src={Calendar} alt="기간" style={{ marginRight: "12px" }} />
+                  설문 날짜
+                </Info_Top_Box>
+                <Info_Bottom_Box>{startTime} ~ {endTime}</Info_Bottom_Box>
+              </Info_Inner_Box>
+            </Info_LR_Box>
+            <Image_Wrapper>
+              <Image src={Kite} alt="연" style={{ transform: "rotate(30deg)", width: "150px", marginLeft: "300px", marginBottom: "20%" }} />
               < Image src={Present} alt="선물상자 " />
-              </Image_Wrapper>
+            </Image_Wrapper>
           </Information_Container>
           <Pay_Container>
             <Info_Inner_Box style={{ alignItems: "flex-start", height: "9%", padding: "0px 12px" }}>
@@ -179,17 +179,17 @@ function Payment(props: any) {
                 <div>
                   {selectedOption.map((selected, index: number) => (
                     <div key={selected.componentKey}>
-                      <ItemBox selectedOption={selected.option} countKey={selected.countKey} handleCountChange={handleCountChange}/>
-                      <Button onClick={() => handleOptionRemove(index)} use="blackwhite" label="삭제하기" style={{alignItems : "center", height : "6%", fontSize : "16px", marginTop : "2px",borderRadius : "4px",border : "3px solid yellow"}} />
-                    
-                </div>
+                      <ItemBox selectedOption={selected.option} countKey={selected.countKey} handleCountChange={handleCountChange} />
+                      <Button onClick={() => handleOptionRemove(index)} use="blackwhite" label="삭제하기" style={{ alignItems: "center", height: "6%", fontSize: "16px", marginTop: "2px", borderRadius: "4px", border: "3px solid yellow" }} />
+
+                    </div>
                   ))}
                 </div>
-                )}
+              )}
             </Selected_Box>
-            <Info_Inner_Box style={{alignItems : "center", height : "6%", fontSize : "18px", marginTop : "10px"}}>총 금액 : {price}원</Info_Inner_Box>
+            <Info_Inner_Box style={{ alignItems: "center", height: "6%", fontSize: "18px", marginTop: "10px" }}>총 금액 : {price}원</Info_Inner_Box>
             <div style={{ width: "90%", height: "8%" }}>
-                 <Button onClick={handlePaymentButtonClick} use="longYellow" label="결제하기" />
+              <Button onClick={handlePaymentButtonClick} use="longYellow" label="결제하기" />
             </div>
           </Pay_Container>
         </Bottom_Container>
