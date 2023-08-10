58f8f481fa5383634d978bb69b75cb00aa7316df
0214kbs <0214kbs@naver.com>
2023-08-11 03:06:13 +0900

    Feat:모바일용 설문 리스트 가져오는 hook

---
+++ frontend/src/Hooks/mobilelistpage/useNormalHook.tsx
@@ -0,0 +1,50 @@
+import { useState, useEffect } from "react";
+import useTimerHook from "@/Hooks/card/useTimerHook";
+import normalListGet from "@/api/surveylist/normalListGet";
+import userNormalListGet from "@/api/surveylist/userNormalListGet";
+
+const useNormalHook = () => {
+  const [cards, setCards] = useState<any>([]);
+
+  useEffect(() => {
+    const fetchList = async () => {
+      const accessToken = localStorage.getItem("accessToken");
+
+      if (!accessToken) {
+        const data = await normalListGet();
+
+        if (!data) {
+          return <div> loading... </div>;
+        }
+        const sortData = data.map((prev: any) => {
+          return { ...prev, remainTime: useTimerHook(prev.endTime) };
+        });
+        setCards(sortData);
+      } else {
+        const data = await userNormalListGet(accessToken);
+        if (!data) {
+          return <div> loading... </div>;
+        }
+        const sortData = data.map((prev: any) => {
+          return { ...prev, remainTime: useTimerHook(prev.endTime) };
+        });
+        setCards(sortData);
+      }
+    };
+    fetchList();
+
+    const timer = setInterval(() => {
+      setCards((prev: any) => {
+        const data = prev.map((prev: any) => {
+          return { ...prev, remainTime: useTimerHook(prev.endTime) };
+        });
+        return data;
+      });
+    }, 1000);
+
+    return () => clearInterval(timer);
+  }, []);
+
+  return { cards };
+};
+export default useNormalHook;
