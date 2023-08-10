cc90b128677d09b84879abf667e162ab23e79548
0214kbs <0214kbs@naver.com>
2023-08-11 02:39:30 +0900

    Design: Survey Card component Design 수정

--- frontend/src/mobileComponents/surveycard/index.tsx
+++ frontend/src/mobileComponents/surveycard/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { SurveycardType } from "./Surveycard.type";
 import { StyledCard, StyledCardRight, StyledCardHeader, StyledRemainTime, StyledImg, StyledProbability } from "./Surveycard.styled";
 import useSSEHook from "@/Hooks/sse/useSSEHook";
+import moment from "moment";
 
 const Surveycard = (props: SurveycardType) => {
   const images: { [key: string]: string } = {
@@ -11,20 +12,38 @@ const Surveycard = (props: SurveycardType) => {
     COFFEE: "/card/coffee.png",
   };
   const imgsrc = images[props.giveaways];
+
+  // 일반, 타임어택, 즉시당첨 구분 => typeName
+  let typeName = "일반";
+  if (props.type === "INSTANT_WIN") {
+    typeName = "즉시당첨";
+  } else if (props.type === "NORMAL") {
+    const now = moment();
+    const endTime = moment(props.endtime, "YYYY-MM-DD-HH-mm");
+    const diffHours = endTime.diff(now, "hours");
+
+    if (diffHours < 24) {
+      typeName = "타임어택";
+    }
+  }
+
+  // 남은 시간 format 설정
   const [unit1, unit2] = props.remaintime ? props.remaintime.split(", ") : ["00분", "00초"];
   const [value1, label1] = unit1.split(":");
   const [value2, label2] = unit2.split(":");
 
-  const percent = useSSEHook(props.id, "확률변동");
-  // const value = percent ? parseFloat(percent) : parseFloat(props.probability);
+  // SSE : 일반 타입인 경우만
+  const percent = props.type === "NORMAL" ? useSSEHook(props.id, "확률변동") : null;
+  const value = percent ? parseFloat(percent) : parseFloat(props.probability);
+  const formattedProbability = value % 1 === 0 ? Math.round(value) + "%" : value.toFixed(1) + "%";
 
-  // const formattedProbability = value % 1 === 0 ? Math.round(value) + "%" : value.toFixed(1) + "%";
+  const newProps = { ...props, typename: typeName };
 
   return (
-    <StyledCard {...props}>
-      <StyledImg src={imgsrc} />
+    <StyledCard {...newProps}>
+      <StyledImg src={imgsrc} {...newProps} />
       <StyledCardRight>
-        <StyledCardHeader {...props}>
+        <StyledCardHeader {...newProps}>
           <div className="title">{props.title}</div>
           <div className="writer" {...props}>
             {props.nickname}
@@ -48,11 +67,24 @@ const Surveycard = (props: SurveycardType) => {
           <div className="text">남음</div>
         </StyledRemainTime>
 
-        <StyledProbability {...props}>
-          <Image src="/card/percent.svg" priority={true} width={20} height={20} alt="remaintime" />
-          <div className="text" {...props}>
-            {props.probability}
-          </div>
+        <StyledProbability {...newProps}>
+          {props.typename === "NORMAL" ? (
+            <Image src="/card/percent.svg" priority={true} width={22} height={22} alt="remaintime" />
+          ) : (
+            <Image src="/card/orangepercent.svg" priority={true} width={22} height={22} alt="remaintime" />
+          )}
+          {props.contentype === "speedy" ? (
+            <div style={{ display: "flex", alignItems: "center" }}>
+              <div className="text" {...newProps}>
+                {props.responsedtime}
+              </div>
+              <div className="speedy"> 소요 </div>
+            </div>
+          ) : (
+            <div className="text" {...newProps}>
+              {formattedProbability}
+            </div>
+          )}
         </StyledProbability>
       </StyledCardRight>
     </StyledCard>
