c73478fc1bedc95b4778b52184e7b8b11e822efc
slbin-park <smpts00@naver.com>
2023-08-10 21:51:57 +0900

    feat: 토큰 재발급 처리 및 로그인 필요한 페이지 url 접근 막는 기능 구현

--- frontend/src/Hooks/user/useLogoutHook.tsx
+++ frontend/src/Hooks/user/useLogoutHook.tsx
@@ -1,17 +1,14 @@
-import { useRouter } from "next/navigation"
-import useUserStore from "@/stores/useUserStore"
+import { useRouter } from "next/navigation";
+import useUserStore from "@/stores/useUserStore";
 export const useLogoutHook = () => {
-    const router = useRouter()
-    const setUserInformation = useUserStore((state:any) => state.setUserInformation)
-    const logout = useUserStore((state:any) => state.logout)
+  const router = useRouter();
+  const logout = useUserStore((state: any) => state.logout);
 
-    const hanedleLogout = () => {
-        router.push("/")
-        localStorage.setItem("accessToken", "")
-        localStorage.setItem("refreshToken", "")
-        logout()
-        setUserInformation(null)
-    }
-    return {hanedleLogout}
-}
-    
+  const hanedleLogout = () => {
+    router.push("/");
+    localStorage.setItem("accessToken", "");
+    localStorage.setItem("refreshToken", "");
+    logout();
+  };
+  return { hanedleLogout };
+};
