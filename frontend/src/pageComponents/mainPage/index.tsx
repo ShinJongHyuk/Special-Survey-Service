c0c2f3c96e4dc2171a89d930e0b127ccf01ce020
0214kbs <0214kbs@naver.com>
2023-08-11 10:01:39 +0900

    Feat: mobile 전환 처리 client 에서

--- frontend/src/pageComponents/mainPage/index.tsx
+++ frontend/src/pageComponents/mainPage/index.tsx
@@ -4,15 +4,23 @@ import Speedyattack from "./components/speedyattack";
 import Banner from "./components/banner";
 import Timeattack from "./components/timeattack";
 import Instantwin from "./components/instantwin";
+import useIsMobileHook from "@/Hooks/useIsMobileHook";
+import Mobilemain from "@/mobilePageComponents/mobilemain";
 
 const MainPage = () => {
-  return (
-    <>
-      <Banner></Banner>
-      <Timeattack />
-      <Speedyattack />
-      <Instantwin />
-    </>
+  const isMobile = useIsMobileHook();
+  return (<>
+    {
+      isMobile ?
+        (<Mobilemain />) : (<>
+          <Banner></Banner>
+          <Timeattack />
+          <Speedyattack />
+          <Instantwin />
+        </>)
+    }
+  </>
+
   );
 };
 
