3122bb19e9cf052626a6f8e9caf638be7c7d924b
0214kbs <0214kbs@naver.com>
2023-08-12 20:59:21 +0900

    Feat: Desktop, Mobile 회원가입페이지 분리

--- frontend/src/pageComponents/viewcheck/index.tsx
+++ frontend/src/pageComponents/viewcheck/index.tsx
@@ -8,6 +8,8 @@ import Surveylist from "../surveylist";
 import Speedyattacklist from "@/mobilePageComponents/speedyattacklist";
 import Mobilelogin from "@/mobilePageComponents/mobilelogin";
 import Login from "../login";
+import Signup from "../signup";
+import MobileSignup from "@/mobilePageComponents/mobilesignup";
 
 const Viewcheck = ({ page, id }: { page: string; id?: string }) => {
   const isMobile = useIsMobileHook();
@@ -17,6 +19,7 @@ const Viewcheck = ({ page, id }: { page: string; id?: string }) => {
     <>
       {page === "main" && (currentView === "desktop" ? <MainPage /> : <Mobilemain />)}
       {page === "login" && (currentView === "desktop" ? <Login /> : <Mobilelogin />)}
+      {page === "signup" && (currentView === "desktop" ? <Signup /> : <MobileSignup />)}
       {page === "surveylist" && (currentView === "desktop" ? <Surveylist /> : <Speedyattacklist from="main" />)}
       {page === "surveydetail" && (currentView === "desktop" ? <SurveyDetail id={id} /> : <MobileSurveyDetail id={id} />)}
     </>
