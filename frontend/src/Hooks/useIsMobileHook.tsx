90a1f07a8ac104d7dca44722d040363ef2b071c6
0214kbs <0214kbs@naver.com>
2023-08-12 16:14:06 +0900

    Feat: device감지, 화면 크기 감지하여 mobile, desktop구분

--- frontend/src/Hooks/useIsMobileHook.tsx
+++ frontend/src/Hooks/useIsMobileHook.tsx
@@ -1,12 +1,13 @@
 "use client";
 import { useState, useEffect } from "react";
+import { isBrowser, isMobile as isMobileDevice } from "react-device-detect";
 
 const useIsMobileHook = () => {
   const [isMobile, setIsMobile] = useState<boolean>(false);
 
   useEffect(() => {
     const handleResize = () => {
-      if (window.innerWidth <= 500) {
+      if (window.innerWidth <= 500 || isMobileDevice) {
         setIsMobile(true);
       } else {
         setIsMobile(false);
