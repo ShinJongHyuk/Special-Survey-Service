3e86d0119fa84f2e85acaf4c6304fbe39eeb40d4
0214kbs <0214kbs@naver.com>
2023-08-14 02:09:31 +0900

    Feat: tab에 따라 컴포넌트 변경 적용, 탭메뉴 postion fixed

--- frontend/src/mobilePageComponents/mypage/index.tsx
+++ frontend/src/mobilePageComponents/mypage/index.tsx
@@ -1,7 +1,32 @@
-import React from "react";
+import Mobiletitle from "@/mobileComponents/mobiletitle";
+import Tabmenu from "@/mobileComponents/tabmenu";
+import { TabType } from "@/mobileComponents/tabmenu/tabmenu.type";
+import { useState } from "react";
+import useAnsweredListHook from "@/Hooks/mypage/useAnsweredListHook";
+import Cardlist from "@/mobileComponents/cardlist";
 
 const MobileMypage = () => {
-  return <div>Mypage</div>;
+  const [selectedTab, setSelectedTab] = useState<TabType>("SURVEY");
+  const { surveys } = useAnsweredListHook();
+
+  return (
+    <div>
+      <div style={{ position: "fixed", width: "100%", zIndex: "0", background: "white" }}>
+        <Mobiletitle text="My Page" />
+        <Tabmenu onTabSelect={(tab: any) => setSelectedTab(tab)} />
+      </div>
+      {selectedTab === "SURVEY" && (
+        <div style={{ paddingTop: "210px" }}>
+          <Cardlist cards={surveys} contentType="mypage"></Cardlist>
+        </div>
+      )}
+      {selectedTab === "PRIZE" && (
+        <div style={{ paddingTop: "210px" }}>
+          <div>PRIZE </div>
+        </div>
+      )}
+    </div>
+  );
 };
 
 export default MobileMypage;
