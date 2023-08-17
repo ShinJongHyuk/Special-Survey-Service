149dbc8a0c0dceef5aa294804dac9249ade0e0f4
0214kbs <0214kbs@naver.com>
2023-08-17 15:49:16 +0900

    Design: 개인정보 동의 페이지 디자인 수정

--- frontend/src/pageComponents/agreement/Agreement.Styled.tsx
+++ frontend/src/pageComponents/agreement/Agreement.Styled.tsx
@@ -11,28 +11,21 @@ const Main_Container = styled.div.attrs({})`
     height : 80%;
 
     justify-content : center;
-    background-image: url("/survey/background_survey7.svg");
-    background-repeat: repeat;
     background-color: white;
 `
 const Information_Container = styled.div.attrs({})`
     display: flex;
-    flex-direction : row;
-    width: 40%;
+    flex-direction: column;
+    width: 480px;
     min-width : 100px;
-    height: 90%;
+    height: 82%;
     align-items : center;
     justify-content : center;
-    flex-wrap: wrap;
 
-    /* background-image: url("/survey/background_survey5.png");
-    background-repeat: repeat; */
     background-color : ${props => props.theme.colors.white};
-    box-shadow : 2px 2px 0px 0px ${props => props.theme.colors.gray} ;
     border : 1px solid ${props => props.theme.colors.lightgray};
     border-radius : 30px;
     padding : 30px 30px 30px 30px;
-    cursor: pointer; 
 
 `;
 
@@ -62,41 +55,41 @@ const StyledTable = styled.table.attrs({})`
 
 `;
 
-const StyledTr = styled.tr.attrs({})`
-`;
+
 
 const StyledTd = styled.td`
     font-size: ${props => props.theme.fontSizes.small};
     background-color: ${props => props.theme.colors.white};
     text-align: center;
     border: 1px solid #dddddd;
-
+    vertical-align: middle;
     padding: 8px;
 `;
 
+const StyledTitleTh = styled.th`
+    background-color: ${props => props.theme.colors.yellow};
+    font-size: ${props => props.theme.fontSizes.small};
+    text-align: center;
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+`;
 const StyledTh = styled.th`
     background-color: ${props => props.theme.colors.yellow};
-    font-size: ${props => props.theme.fontSizes.medium};
+    font-size: ${props => props.theme.fontSizes.small};
     text-align: center;
     border: 1px solid #dddddd;
     text-align: left;
     padding: 8px;
 `;
 
-const StyledThead = styled.thead``;
-const StyledTbody = styled.tbody``;
-const StyledTfoot = styled.tfoot``;
 
 const ButtonBox = styled.div.attrs({})`
     display : flex;
     flex-direction : row;
     width : 90%;
-    height : 15%;
-    gap : '10px';
-    margin-top : 7%;
-
-
+    height : 11%;
 `
 
 
-export {ButtonBox,StyledTbody,StyledTfoot,StyledThead,StyledTr,StyledTd,StyledTable,StyledTh,Title_Text,AgreementPage,Main_Container,Information_Container};
+export { StyledTitleTh, ButtonBox, StyledTd, StyledTable, StyledTh, Title_Text, AgreementPage, Main_Container, Information_Container };
